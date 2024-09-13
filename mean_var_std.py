003beb23da35dce2c46b7998b1a39322afa9b55b
Lucas-nepomuceno <158762017+Lucas-nepomuceno@users.noreply.github.com>
2024-09-13 09:55:44 -0300

    feat: creates function calculate()

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,18 @@
 import numpy as np
 
+
 def calculate(list):
-
-
-
-
-    return calculations
+    if(len(list) != 9):
+        raise ValueError('List must contain nine numbers.')
+    else:
+        matrix = np.array(list).reshape((3,3))
+        flattened = np.array(list)
+        calculations = {
+            'mean': [np.mean(matrix, axis = 0).tolist(), np.mean(matrix, axis = 1).tolist(), np.mean(flattened)],
+            'variance': [np.var(matrix, axis = 0).tolist(), np.var(matrix, axis = 1).tolist(), np.var(flattened)],
+            'standard deviation': [np.std(matrix, axis = 0).tolist(), np.std(matrix, axis = 1).tolist(), np.std(flattened)],
+            'max': [np.max(matrix, axis = 0).tolist(), np.max(matrix, axis = 1).tolist(), np.max(flattened)],
+            'min': [np.min(matrix, axis = 0).tolist(), np.min(matrix, axis = 1).tolist(), np.min(flattened)],
+            'sum': [np.sum(matrix, axis = 0).tolist(), np.sum(matrix, axis = 1).tolist(), np.var(flattened)]
+        }
+        return calculations
